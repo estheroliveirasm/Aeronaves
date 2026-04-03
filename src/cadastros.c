ce5ce3b3f30b3e9b28ee728ea9d1d6f01cdacb3c
estheroliveirasm <estherdeoliveirasm@gmail.com>
2026-04-03 16:56:11 -0300

    mudanca na data

--- src/cadastros.c
+++ src/cadastros.c
@@ -4,12 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../include/cadastros.h"
-#include "../include/utils.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 dados_aeronaves_t *nova_aeronave() {
     dados_aeronaves_t *aeronave = (dados_aeronaves_t*)malloc(sizeof(dados_aeronaves_t));
     int num_tipo, num_status;
@@ -17,54 +11,53 @@ dados_aeronaves_t *nova_aeronave() {
     
     aeronave->qtd_manutencoes = 0;
     printf("......CADASTRO DE AERONAVE.......\n");
-    printf("Identificacao da aeronave......: ");
-    scanf("%i", &aeronave->id_aeronave);
+    printf("Identificacao da aeronave...........: ");
+    scanf("%d", &aeronave->id_aeronave);
     while (getchar() != '\n');
 
-    printf("Modelo da aeronave.............: ");
+    printf("Modelo da aeronave..................: ");
     fgets(aeronave->modelo, STR_SIZE, stdin);
     remover_enter(aeronave->modelo);
-    to_upper_string(aeronave->modelo);
+    to_upper_string(aeronave->modelo); 
 
-    printf("Fabricante da aeronave.........: ");
+    printf("Fabricante da aeronave..............: ");
     fgets(aeronave->fabricante, STR_SIZE, stdin);
     remover_enter(aeronave->fabricante);
-    to_upper_string(aeronave->fabricante);
+    to_upper_string(aeronave->fabricante); //
 
-    printf("Matricula do aviao.............: ");
+    printf("Matricula do aviao..................: ");
     fgets(aeronave->matricula, STR_SIZE, stdin);
     remover_enter(aeronave->matricula);
-    to_upper_string(aeronave->matricula);
+    to_upper_string(aeronave->matricula); //
 
-    printf("Ano de fabricacao..............: ");
-    scanf("%i", &aeronave->ano_fabricacao);
+    printf("Ano de fabricacao...................: ");
+    scanf("%d", &aeronave->ano_fabricacao);
     
-    printf("Tipo (1- CARGA | 2- PASSAGEIRO): ");
-    scanf("%i", &num_tipo);
+    printf("Tipo (1- CARGA | 2- PASSAGEIRO).....: ");
+    scanf("%d", &num_tipo);
     while (getchar() != '\n');
     aeronave->tipo = (tipo_aeronave_t)num_tipo;
 
-    // CORREÇÃO DOS NOMES: num_passageiros e qtd_carga_util
     if(aeronave->tipo == PASSAGEIRO) {
-        printf("Numero de passageiros..........: ");
-        scanf("%i", &aeronave->num_passageiros);
+        printf("Numero de passageiros...............: ");
+        scanf("%d", &aeronave->num_passageiros);
         aeronave->qtd_carga_util = 0;
     } else { 
         aeronave->num_passageiros = 0;
-        printf("Quantidade de carga util (kg)..: ");
-        scanf("%i", &aeronave->qtd_carga_util);
+        printf("Quantidade de carga util (kg).......: ");
+        scanf("%d", &aeronave->qtd_carga_util);
     }
     while (getchar() != '\n');
 
     printf("Status (1- OPERACAO | 2- MANUTENCAO): ");
-    scanf("%i", &num_status);
+    scanf("%d", &num_status);
     while (getchar() != '\n');
     aeronave->status = (status_t)num_status;
     
     if(aeronave->status == MANUTENCAO) aeronave->qtd_manutencoes++;
 
-    printf("Tripulacao necessaria..........: ");
-    scanf("%i", &aeronave->num_tripulantes);
+    printf("Tripulacao necessaria...............: ");
+    scanf("%d", &aeronave->num_tripulantes);
     while (getchar() != '\n');
 
     aeronave->prox = NULL;
@@ -83,7 +76,7 @@ dados_rotas_t *nova_rota(dados_aeronaves_t *lista_aeronaves) {
 
     do {
         printf("\nAeronave a ser alocada (ID): ");
-        scanf("%i", &rota->id_aeronave);
+        scanf("%d", &rota->id_aeronave);
         while (getchar() != '\n');
         aero_ref = localizar_aeronave_por_id(rota->id_aeronave, lista_aeronaves);
 
@@ -95,59 +88,64 @@ dados_rotas_t *nova_rota(dados_aeronaves_t *lista_aeronaves) {
     printf("Codigo da rota..................: ");
     scanf("%d", &rota->codigo_rota);
     while (getchar() != '\n');
-    printf("Data (dd/mm/aaaa)...............: ");
-    scanf("%i/%i/%i", &rota->data_e_hora.dia, &rota->data_e_hora.mes, &rota->data_e_hora.ano);
+
+    printf("Data (DD/MM/AAAA): ");
+    scanf("%d/%d/%d", &rota->data_e_hora.dia, &rota->data_e_hora.mes, &rota->data_e_hora.ano);
     while (getchar() != '\n');
-    printf("Horario (hh:mm).................: ");
-    scanf("%i:%i", &rota->data_e_hora.hora, &rota->data_e_hora.min);
+
+    printf("Horario (HH:MM): ");
+    scanf("%d:%d", &rota->data_e_hora.hora, &rota->data_e_hora.min);
     while (getchar() != '\n');
 
     printf("Local de partida................: ");
     fgets(rota->local_partida, STR_SIZE, stdin);
     remover_enter(rota->local_partida);
+    to_upper_string(rota->local_partida); // Novo: Maiúsculas
+
     printf("Local de destino................: ");
     fgets(rota->local_destino, STR_SIZE, stdin);
     remover_enter(rota->local_destino);
+    to_upper_string(rota->local_destino); // Novo: Maiúsculas
 
-    // FORMATO CORRETO PARA NUMEROS GRANDES (45000.00)
     printf("Combustivel necessario (litros).: ");
     scanf("%f", &rota->combustivel_voo);
     while (getchar() != '\n');
 
     if (aero_ref->tipo == PASSAGEIRO) {
         do {
-            printf("Quantidade de passageiros (Max %i): ", aero_ref->num_passageiros);
-            scanf("%i", &rota->qtd_passageiros);
+            printf("Quantidade de passageiros (Max %d): ", aero_ref->num_passageiros);
+            scanf("%d", &rota->qtd_passageiros);
             while (getchar() != '\n');
         } while(rota->qtd_passageiros > aero_ref->num_passageiros);
         rota->carga_util = 0;
     } else {
         do {
-            printf("Quantidade de carga kg (Max %i): ", aero_ref->qtd_carga_util);
-            scanf("%i", &rota->carga_util);
+            printf("Quantidade de carga kg (Max %d): ", aero_ref->qtd_carga_util);
+            scanf("%d", &rota->carga_util);
             while (getchar() != '\n');
         } while(rota->carga_util > aero_ref->qtd_carga_util);
         rota->qtd_passageiros = 0;
     }
 
     do {
-        printf("Numero de tripulantes (Min %i): ", aero_ref->num_tripulantes);
-        scanf("%i", &rota->num_membros);
+        printf("Numero de tripulantes (Min %d): ", aero_ref->num_tripulantes);
+        scanf("%d", &rota->num_membros);
         while (getchar() != '\n');
     } while (rota->num_membros < aero_ref->num_tripulantes || rota->num_membros > 10);
 
     for(int i = 0; i < rota->num_membros; i++) {
-        printf("Nome do membro %i: ", i + 1);
+        printf("Nome do membro %d: ", i + 1);
         fgets(rota->nomes_membros[i], STR_SIZE, stdin);
         remover_enter(rota->nomes_membros[i]);
+        to_upper_string(rota->nomes_membros[i]); // Novo: Maiúsculas
     }
 
     rota->prox = NULL;
     return rota;
 }
+// ... (resto das funções de busca e inserção permanecem iguais)
 
-void inserir_aeronave_lista_pelo_fim(dados_aeronaves_t **lista, dados_aeronaves_t *nova_aeronave)
-{
+void inserir_aeronave_lista_pelo_fim(dados_aeronaves_t **lista, dados_aeronaves_t *nova_aeronave) {
     if (nova_aeronave == NULL) return;
     if (*lista == NULL) {
         *lista = nova_aeronave;
@@ -158,8 +156,7 @@ void inserir_aeronave_lista_pelo_fim(dados_aeronaves_t **lista, dados_aeronaves_
     }
 }
 
-void inserir_rota_lista_pelo_fim(dados_rotas_t **lista, dados_rotas_t *nova_rota)
-{
+void inserir_rota_lista_pelo_fim(dados_rotas_t **lista, dados_rotas_t *nova_rota) {
     if (nova_rota == NULL) return;
     if (*lista == NULL) {
         *lista = nova_rota;
@@ -170,21 +167,19 @@ void inserir_rota_lista_pelo_fim(dados_rotas_t **lista, dados_rotas_t *nova_rota
     }
 }
 
-dados_aeronaves_t *localizar_aeronave_por_id(int id_aeronave, dados_aeronaves_t *lista)
-{
+dados_aeronaves_t *localizar_aeronave_por_id(int id_aeronave, dados_aeronaves_t *lista) {
    if(lista == NULL) return NULL;
    if(lista->id_aeronave == id_aeronave) return lista;
    return localizar_aeronave_por_id(id_aeronave, lista->prox);
 }
 
-void alterar_status_aeronave(dados_aeronaves_t **lista_aeronaves)
-{
+void alterar_status_aeronave(dados_aeronaves_t **lista_aeronaves) {
     int id_escolhido, novo_status;
     dados_aeronaves_t *aeronave = NULL;
 
     printf("ALTERAR STATUS DA AERONAVE\n");
-    printf("Digite o ID da aeronave: \n");
-    scanf("%i", &id_escolhido);
+    printf("Digite o ID da aeronave: ");
+    scanf("%d", &id_escolhido);
     while (getchar() != '\n');
 
     aeronave = localizar_aeronave_por_id(id_escolhido, *lista_aeronaves);
@@ -195,16 +190,16 @@ void alterar_status_aeronave(dados_aeronaves_t **lista_aeronaves)
     }
 
     printf("Digite o novo status(1 - Em operação ou 2- Em manutenção): ");
-    scanf("%i", &novo_status);
+    scanf("%d", &novo_status);
     while (getchar() != '\n');
 
     if((status_t)novo_status == MANUTENCAO && aeronave->status != MANUTENCAO) {
         aeronave->qtd_manutencoes++;
-        printf("Aeronave entrou em manutenção pela %iª vez.\n", aeronave->qtd_manutencoes);
+        printf("Aeronave entrou em manutenção pela %dª vez.\n", aeronave->qtd_manutencoes);
     }
 
     aeronave->status = (status_t)novo_status;
-    printf("Status da aeronave ID: %i alterado para %s.\n", id_escolhido, 
+    printf("Status da aeronave ID: %d alterado para %s.\n", id_escolhido, 
           (aeronave->status == OPERACAO) ? "EM OPERAÇÃO" : "EM MANUTENÇÃO");
     mensagem("Pressione ENTER para continuar!");
 }
--- src/relatorios.c
+++ src/relatorios.c
@@ -2,162 +2,125 @@
 #include "../include/utils.h"
 #include <stdio.h>
 
-void mostrar_dados_aeronaves(dados_aeronaves_t *aeronave, FILE *fp)
-{
+void mostrar_dados_aeronaves(dados_aeronaves_t *aeronave, FILE *fp) {
     fprintf(fp, "\n......DADOS DA AERONAVE......\n");
-    fprintf(fp, "Identificação........: %i\n", aeronave->id_aeronave);
-    fprintf(fp, "Modelo...............: %s\n", aeronave->modelo);
-    fprintf(fp,"Fabricante...........: %s\n", aeronave->fabricante);
-    fprintf(fp, "Matrícula............: %s\n", aeronave->matricula);
-    fprintf(fp, "Ano de fabricação....: %i\n", aeronave->ano_fabricacao);
+    fprintf(fp, "IDENTIFICACAO........: %d\n", aeronave->id_aeronave);
+    fprintf(fp, "MODELO...............: %s\n", aeronave->modelo);
+    fprintf(fp, "FABRICANTE...........: %s\n", aeronave->fabricante);
+    fprintf(fp, "MATRICULA............: %s\n", aeronave->matricula);
+    fprintf(fp, "ANO DE FABRICACAO....: %d\n", aeronave->ano_fabricacao);
     
     if(aeronave->tipo == CARGA){
-        fprintf(fp, "Tipo.................: CARGA (Capacidade: %i)\n", aeronave->qtd_carga_util);
-    }
-    else{
-        fprintf(fp, "Tipo.................: PASSAGEIROS (Capacidade: %i)\n", aeronave->num_passageiros);
+        fprintf(fp, "TIPO.................: CARGA (CAPACIDADE: %d KG)\n", aeronave->qtd_carga_util);
+    } else {
+        fprintf(fp, "TIPO.................: PASSAGEIROS (CAPACIDADE: %d)\n", aeronave->num_passageiros);
     }
 
     if(aeronave->status == OPERACAO){
-        fprintf(fp, "Status...............: EM OPERAÇÃO\n");
+        fprintf(fp, "STATUS...............: EM OPERACAO\n");
+    } else {
+        fprintf(fp, "STATUS...............: EM MANUTENCAO (QTD. MANUTENCOES: %d)\n", aeronave->qtd_manutencoes);
     }
-    else{
-        fprintf(fp, "Status...............: EM MANUTENÇÃO (Qtd. Manutenções: %i)\n", aeronave->qtd_manutencoes);
-    }
-
-    fprintf(fp, "Tripulantes necessários: %i\n", aeronave->num_tripulantes);
+    fprintf(fp, "TRIPULANTES NECESSARIOS: %d\n", aeronave->num_tripulantes);
 }
 
-void mostrar_dados_rotas(dados_rotas_t *rota, FILE *fp)
-{
+void mostrar_dados_rotas(dados_rotas_t *rota, FILE *fp) {
     fprintf(fp, "\n......DADOS DA ROTA......\n");
-    fprintf(fp, "Código da rota..................: %d\n", rota->codigo_rota);
-    fprintf(fp, "Aeronave alocada................: %i\n", rota->id_aeronave);
-    fprintf(fp, "Data do voo.....................: %02i/%02i/%02i \n", rota->data_e_hora.dia, rota->data_e_hora.mes, rota->data_e_hora.ano);
-    fprintf(fp, "Horario do voo..................: %i:%i \n", rota->data_e_hora.hora, rota->data_e_hora.min);
-    fprintf(fp, "Local de partida................: %s\n", rota->local_partida);        
-    fprintf(fp, "Local de destino................: %s\n", rota->local_destino);
-    fprintf(fp, "Tempo estimado de voo...........: %.2f horas\n", rota->tempo_estimado);
-    fprintf(fp, "Combustivel necessário..........: %.2f litros\n", rota->combustivel_voo);
-    fprintf(fp, "Quantidade de passageiros.......: %i\n", rota->qtd_passageiros);
-    fprintf(fp, "Quantidade de carga util........: %i\n", rota->carga_util);
-    fprintf(fp, "Número de tripulantes...........: %i\n", rota->num_membros);
+    fprintf(fp, "CODIGO DA ROTA..................: %d\n", rota->codigo_rota);
+    fprintf(fp, "AERONAVE ALOCADA................: %d\n", rota->id_aeronave);
+    fprintf(fp, "DATA DO VOO.....................: %02d/%02d/%04d\n", 
+            rota->data_e_hora.dia, rota->data_e_hora.mes, rota->data_e_hora.ano);
+    fprintf(fp, "HORARIO DO VOO..................: %02d:%02d\n", 
+            rota->data_e_hora.hora, rota->data_e_hora.min);
+    fprintf(fp, "LOCAL DE PARTIDA................: %s\n", rota->local_partida);        
+    fprintf(fp, "LOCAL DE DESTINO................: %s\n", rota->local_destino);
+    fprintf(fp, "COMBUSTIVEL NECESSARIO..........: %.2f LITROS\n", rota->combustivel_voo);
+    fprintf(fp, "QUANTIDADE DE PASSAGEIROS.......: %d\n", rota->qtd_passageiros);
+    fprintf(fp, "QUANTIDADE DE CARGA UTIL........: %d\n", rota->carga_util);
+    fprintf(fp, "NUMERO DE TRIPULANTES...........: %d\n", rota->num_membros);
 
     for(int i = 0; i < rota->num_membros; i++){
-        fprintf(fp, "  - Membro %i: %s\n", i + 1, rota->nomes_membros[i]);
+        fprintf(fp, "  - MEMBRO %d: %s\n", i + 1, rota->nomes_membros[i]);
     }
 }
+// ... (resto das funções de listagem permanecem iguais)
 
-void relatorio_aeronaves(dados_aeronaves_t *lista, FILE *fp)
-{
+void relatorio_aeronaves(dados_aeronaves_t *lista, FILE *fp) {
     dados_aeronaves_t *atual = lista;
     int contador = 0;
-
-    printf("......RELATÓRIO DE AERONAVES CADASTRADAS......\n");
-
     if(lista == NULL){
         printf("Nenhuma aeronave cadastrada!\n");
         mensagem("Pressione ENTER para continuar!");
         return;
     }
-
+    printf("......RELATÓRIO DE AERONAVES CADASTRADAS......\n");
     while(atual != NULL) {
-        printf("\n--- AERONAVE %i ---\n", contador + 1);
+        printf("\n--- AERONAVE %d ---\n", contador + 1);
         mostrar_dados_aeronaves(atual, stdout);
         contador++;
         atual = atual->prox;
     }
-
-    printf("\nTotal de aeronaves: %i\n", contador); 
+    printf("\nTotal de aeronaves: %d\n", contador); 
     mensagem("Pressione ENTER para continuar!");
 }
 
-void relatorio_rotas(dados_rotas_t *lista, FILE *fp)
-{
+void relatorio_rotas(dados_rotas_t *lista, FILE *fp) {
     dados_rotas_t *atual = lista;
     int contador = 0;
-
-    printf("......RELATÓRIO DE ROTAS CADASTRADAS......\n");
-
     if(lista == NULL){
         printf("Nenhuma rota cadastrada!\n");
         mensagem("Pressione ENTER para continuar!");
         return;
     }
-
+    printf("......RELATÓRIO DE ROTAS CADASTRADAS......\n");
     while(atual != NULL) {
-        printf("\n--- ROTA %i ---\n", contador + 1);
+        printf("\n--- ROTA %d ---\n", contador + 1);
         mostrar_dados_rotas(atual, stdout);
         contador++;
         atual = atual->prox;
     }
-
-    printf("\nTotal de rotas: %i\n", contador); 
+    printf("\nTotal de rotas: %d\n", contador); 
     mensagem("Pressione ENTER para continuar!");
 }
 
-void listar_rotas_maiornumero_passageiros(dados_rotas_t *lista, FILE *fp)
-{
+void listar_rotas_maiornumero_passageiros(dados_rotas_t *lista, FILE *fp) {
     int max_passageiros = -1;
     dados_rotas_t *atual = lista;
-
-    printf("......ROTAS COM O MAIOR NÚMERO DE PASSAGEIROS......\n");
-
     if(lista == NULL){
         printf("Nenhuma rota cadastrada!");
         mensagem("Pressione ENTER para continuar!");
         return;
     }
-
     while(atual != NULL) {
-        if(atual->qtd_passageiros > max_passageiros){
-            max_passageiros = atual->qtd_passageiros;
-        }
+        if(atual->qtd_passageiros > max_passageiros) max_passageiros = atual->qtd_passageiros;
         atual = atual->prox;
     }
-    
-    printf("Maior número de passageiros em uma rota: %i\n", max_passageiros);
-
+    printf("......ROTAS COM O MAIOR NÚMERO DE PASSAGEIROS (%d)......\n", max_passageiros);
     atual = lista; 
     while(atual != NULL) {
-        if(atual->qtd_passageiros == max_passageiros){
-            mostrar_dados_rotas(atual, stdout);
-        }
+        if(atual->qtd_passageiros == max_passageiros) mostrar_dados_rotas(atual, stdout);
         atual = atual->prox;
     }
-
     mensagem("Pressione ENTER para continuar!");
 }
 
-void listar_rotas_menornumero_passageiros(dados_rotas_t *lista, FILE *fp)
-{
+void listar_rotas_menornumero_passageiros(dados_rotas_t *lista, FILE *fp) {
     int min_passageiros = 2147483647; 
     dados_rotas_t *atual = lista;
-
-    printf("......ROTAS COM O MENOR NÚMERO DE PASSAGEIROS......\n");
-
     if(lista == NULL){
         printf("Nenhuma rota cadastrada!");
         mensagem("Pressione ENTER para continuar!");
         return;
     }
-
     while(atual != NULL) {
-        if(atual->qtd_passageiros < min_passageiros){
-            min_passageiros = atual->qtd_passageiros;
-        }
+        if(atual->qtd_passageiros < min_passageiros) min_passageiros = atual->qtd_passageiros;
         atual = atual->prox;
     }
-
-    printf("Menor número de passageiros em uma rota: %i\n", min_passageiros);
-
+    printf("......ROTAS COM O MENOR NÚMERO DE PASSAGEIROS (%d)......\n", min_passageiros);
     atual = lista; 
     while(atual != NULL) {
-        if(atual->qtd_passageiros == min_passageiros){
-            mostrar_dados_rotas(atual, stdout);
-        }
+        if(atual->qtd_passageiros == min_passageiros) mostrar_dados_rotas(atual, stdout);
         atual = atual->prox;
     }
-
     mensagem("Pressione ENTER para continuar!");
 }
